a665dd638eb2e321fe2fd703c6f0814c8962a692
manish gupta <manish.gta1@gmail.com>
2022-12-29 22:52:22 +0530

    connected to mongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const { graphqlHTTP }  = require('express-graphql');
 const schema = require('./schema/schema')
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+
+mongoose.connection.once('open',() => {
+  console.log('connected to DB');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
